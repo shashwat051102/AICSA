8134ac5673b188e1cf9ef32aa08b45e4fd471e3b
shashwat051102 <137853743+shashwat051102@users.noreply.github.com>
2025-05-15 00:46:40 +0530

    Update PDF_rag_tool.py

--- Tools/PDF_rag_tool.py
+++ Tools/PDF_rag_tool.py
@@ -1,24 +1,4 @@
 from crewai_tools import PDFSearchTool
 
 # Initialize the tool allowing for any PDF content search if the path is provided during execution
-PDF_tool = PDFSearchTool(
-    config=dict(
-        llm=dict(
-            provider="ollama", # or google, openai, anthropic, llama2, ...
-            config=dict(
-                model="llama2",
-                # temperature=0.5,
-                # top_p=1,
-                # stream=true,
-            ),
-        ),
-        embedder=dict(
-            provider="google", # or openai, ollama, ...
-            config=dict(
-                model="models/embedding-001",
-                task_type="retrieval_document",
-                # title="Embeddings",
-            ),
-        ),
-    )
-)
+PDF_tool = PDFSearchTool()
